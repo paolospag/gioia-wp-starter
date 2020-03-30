09bafd07c77e374cbf6c3a4825bc1b48b0e7c4fb
paolospag <paolo@marcosh.net>
2020-03-30 18:06:21 +0200

    Make index.php more HTML-ish

--- index.php
+++ index.php
@@ -14,17 +14,17 @@ get_header();
 ?>
 
 <div class="container">
-  <?php
-    if( have_posts() ):
-      echo '<div class="posts-list" role="list">';
-      while( have_posts() ): the_post();
-        get_template_part('template-parts/content');
-      endwhile;
-      echo '</div>';
-    else:
-      echo '<p>'. __('Nessun articolo trovato.', '%DOMAIN_NAME%') .'</p>';
-    endif;
-  ?>
+  <?php if( have_posts() ): ?>
+    <div class="posts-list" role="list">
+      <?php
+        while( have_posts() ): the_post();
+          get_template_part('template-parts/content');
+        endwhile;
+      ?>
+    </div>
+  <?php else: ?>
+    <p><?php _e('Nessun articolo trovato.', '%DOMAIN_NAME%'); ?></p>
+  <?php endif; ?>
 </div>
 
 <?php
